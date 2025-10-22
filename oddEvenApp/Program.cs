304e4d3e1e1f874ed76b625c94a7c7d372dd0e4f
andro <andro@gmail.com>
2025-10-22 06:29:03 +0300

    init commit

---
+++ oddEvenApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+Console.WriteLine("Hello, World!");
+
+
+print(arr); 
+
+static void  print(int[] arr)
+{
+    foreach(int i in arr)
+        Console.Write($"{i}  ");
+}
+
+static bool even(int num)
+{
+    if(num %2 == 0)
+        return true;    
+    else
+        return false;   
+}
+
+static bool odd(int num)
+{
+    if (num % 2 != 0)
+        return true;
+    else
+        return false;
+}
